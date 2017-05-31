e9e325ce26c0f791ae1e6a682892cee82638fcfa
carlos henrique da silva melo <carlossilvamelo7@hotmail.com>
2017-05-31 14:27:34 -0300

    estrutura do projeto

---
+++ src/main/java/com/SGSRveiculo/SgsRveiculoApplication.java
@@ -0,0 +1,12 @@
+package com.SGSRveiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsRveiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsRveiculoApplication.class, args);
+	}
+}
